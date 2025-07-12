e524a7d087c0876e2f3f81ec72de8573973c9948
marcellyluizeto <maluizeto@gmail.com>
2025-07-12 11:55:32 -0300

    Update organismo_fasta.py

--- bio/organismo_fasta.py
+++ bio/organismo_fasta.py
@@ -1,4 +1,8 @@
 class OrganismoFasta:
-    """
-    Criar
-    """
+
+    def __init__(self, id, nome, sequencia):
+        self.id = id 
+        self.nome = nome
+        self.sequencia = sequencia 
+        
+    
