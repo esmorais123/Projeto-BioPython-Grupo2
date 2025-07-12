aa581d12724e856b7922b9def4d4ff355bfe3cd3
fesilveiraa <fesilveira23@gmail.com>
2025-07-12 14:59:33 -0300

    metodo percentual

--- bio/sequencia.py
+++ bio/sequencia.py
@@ -23,3 +23,15 @@ class Sequencia:
 
     def calcular_tamanho(self):
         return len(self.sequencia)
+    
+    def percentual(self, bases):
+        tamanho = self.calcular_tamanho()
+        count = 0
+        for base_especifica in self.sequencia:
+            if base_especifica in bases:
+                count = count + 1 
+        
+        percentual = count / tamanho
+        return percentual 
+
+      
