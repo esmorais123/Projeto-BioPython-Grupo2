7c2f2480f627728adff6061e56f914febd311526
Eric <ericsm123@gmail.com>
2025-07-12 12:40:05 -0300

    Passando sequencia como argumento

--- bio/organismo_fasta.py
+++ bio/organismo_fasta.py
@@ -1,6 +1,8 @@
+from bio.sequencia import Sequencia
+
 class OrganismoFasta:
 
     def __init__(self, id, nome, sequencia):
         self.id = id
         self.nome = nome
-        self.sequencia = sequencia
+        self.sequencia = Sequencia(sequencia)
