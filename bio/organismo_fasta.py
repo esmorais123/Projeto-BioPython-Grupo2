c2f2e5ace2de438943758194ca5e488e7eb31ca5
Eric <ericsm123@gmail.com>
2025-07-12 11:54:32 -0300

    Criação do organismo_fasta

--- bio/organismo_fasta.py
+++ bio/organismo_fasta.py
@@ -1,4 +1,6 @@
 class OrganismoFasta:
-    """
-    Criar
-    """
+    
+    def __init__(self, id, nome, sequencia):
+        self.id = id
+        self.nome = nome
+        self.sequencia = sequencia
