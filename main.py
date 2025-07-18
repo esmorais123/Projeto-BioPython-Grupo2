571ff5644193a5c7e707bf50f5de931e4291d08a
marcellyluizeto <maluizeto@gmail.com>
2025-07-18 15:52:19 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-
-
 from bio.ler_fasta import ler_fasta
 
 caminho_do_arquivo = "./arquivos/Flaviviridae-genomes.fasta"
 
 organismo_do_fasta = ler_fasta(caminho_do_arquivo)
 
-print ("Os organismos desse arquivo fasta são: ")
 for organismo in organismo_do_fasta:
-    print(organismo.nome)
+    print("sequência original:", organismo.sequencia)
+    print("proteína traduzida:", organismo.traduzir(parar=False))
