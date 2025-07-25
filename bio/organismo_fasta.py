3b67dfd1e36034409154c3dbd713aa77001352c4
Eric <ericsm123@gmail.com>
2025-07-24 21:08:39 -0300

    Troca de lugar da função.

--- bio/organismo_fasta.py
+++ bio/organismo_fasta.py
@@ -7,16 +7,3 @@ class OrganismoFasta:
         self.id = id
         self.nome = nome
         self.sequencia = Sequencia(sequencia)
-
-    def traduzir(self, parar=False):
-        proteina = ""
-
-        for i in range(0, len(self.sequencia) - 2, 3):
-            codon = self.sequencia[i:i+3]
-            aminoacido = DNA_PARA_AMINOACIDO.get(codon, "X")  
-            if parar and aminoacido == "*":
-                break
-
-            proteina += aminoacido
-
-        return proteina
